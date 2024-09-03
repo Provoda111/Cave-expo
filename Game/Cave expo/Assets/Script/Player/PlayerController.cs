4a2d2326f332917950ad4ced564a5ed2c8a95c77
Provoda111 <provodochek2@gmail.com>
2024-09-03 14:09:16 +0300

    Added models, prefabs and etc.

--- Game/Cave expo/Assets/Script/Player/PlayerController.cs
+++ Game/Cave expo/Assets/Script/Player/PlayerController.cs
@@ -1,49 +1,84 @@
+using RPGCharacterAnims.Actions;
+using RPGCharacterAnims.Extensions;
+using RPGCharacterAnims.Lookups;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
+using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
+
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
-    [SerializeField] private float playerSpeed = 50.5f;
-    [SerializeField] private float playerJump = 5f;
+    [SerializeField] private float playerSpeed = 5f;
+    [SerializeField] private float playerJump = 20f;
     public Vector3 movement;
-    public Transform mainCamera;
-    [SerializeField] private float cameraTurnSpeed = 20.0f;
     private float horizontalMovement; private float verticalMovement;
+    public Animator animator;
 
-    public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
+    private bool isGrounded;
+
+    private float yaw = 0f;
+    public Camera playerCamera;
+    public float mouseSensitivity = 2f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
+        animator = GetComponent<Animator>();
     }
     // Update is called once per frame
-    void Update()
+    async void Update()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            //playerSpeed += 2.5f;
+        }
         horizontalMovement = Input.GetAxis("Horizontal"); verticalMovement = Input.GetAxis("Vertical");
         movement = new Vector3(horizontalMovement, 0.0f, verticalMovement).normalized;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.forward * playerJump, ForceMode.Impulse);
+            //rb.AddForce(new Vector3(0, 100, 0), ForceMode.Impulse);
+            isGrounded = false;
+            animator.SetBool("isJumping", true);
+            
+            animator.SetBool("isJumping", false);
+            isGrounded = true;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("isWalking", true);
+        }
+        moveCharacter();
+        HandleCameraRotation();
+        HandleAnimations();
+    }
+    void moveCharacter()
+    {
+        Vector3 worldMovement = transform.TransformDirection(movement);
+        rb.MovePosition(transform.position + worldMovement * playerSpeed * Time.fixedDeltaTime);
+    }
+    private void HandleCameraRotation()
+    {
+        if (Input.GetButton("Fire2"))
+        {
+            yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+            playerCamera.transform.localRotation = Quaternion.Euler(0, yaw, 0);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    protected void HandleAnimations()
+    {
         
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            // JUMP
-            rb.AddForce(Vector3.up * playerJump, ForceMode.Impulse);
-        }
-    }
-    void FixedUpdate()
-    {
-        moveCharacter(movement);
-        if (movement.magnitude > 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg + mainCamera.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity);
-        }
-    }
-    void moveCharacter(Vector3 direction)
-    {
-        rb.velocity = direction * playerSpeed * Time.deltaTime;
     }
 }
