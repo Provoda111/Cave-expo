b8f20177daa32dfd1e1d972787d7afc175764584
Provoda111 <provodochek2@gmail.com>
2024-09-06 12:55:26 +0300

    Added new animations
    New animations, updated and upgraded code, created new Methods (void)

---
+++ Game/Cave expo/Assets/Script/Player/PlayerCheckpoint.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCheckpoint : MonoBehaviour
+{
+    //public Transform player;
+    public Transform[] playerCheckpoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Game/Cave expo/Assets/Script/Player/PlayerController.cs
+++ Game/Cave expo/Assets/Script/Player/PlayerController.cs
@@ -3,6 +3,7 @@ using RPGCharacterAnims.Extensions;
 using RPGCharacterAnims.Lookups;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEditor.Experimental.GraphView;
 using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
@@ -12,72 +13,120 @@ using static UnityEditor.Searcher.SearcherWindow.Alignment;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
-    [SerializeField] private float playerSpeed = 5f;
-    [SerializeField] private float playerJump = 20f;
-    public Vector3 movement;
-    private float horizontalMovement; private float verticalMovement;
     public Animator animator;
+    public Transform playerCamera;
+    private PlayerCheckpoint playerCheckpoint;
+    public Transform sword;
+
+    [SerializeField] private float playerSpeed = 5f;
+    [SerializeField] private float playerRotation = 720f;
+    [SerializeField] private float playerJump = 20f;
+    [SerializeField] private int playerAttack = 5;
+    [SerializeField] private float playerHP = 100f;
+    [SerializeField] private bool shieldProtect = false;
+
+    public Vector3 movement;
+    public Vector3 moveDirection;
 
     private bool isGrounded;
-
-    private float yaw = 0f;
-    public Camera playerCamera;
-    public float mouseSensitivity = 2f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
+        playerCheckpoint = GetComponent<PlayerCheckpoint>();
+        Debug.Log($"isGrounded {isGrounded}");
     }
-    // Update is called once per frame
-    async void Update()
+    void Update()
     {
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            //playerSpeed += 2.5f;
-        }
-        horizontalMovement = Input.GetAxis("Horizontal"); verticalMovement = Input.GetAxis("Vertical");
-        movement = new Vector3(horizontalMovement, 0.0f, verticalMovement).normalized;
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.AddForce(Vector3.forward * playerJump, ForceMode.Impulse);
-            //rb.AddForce(new Vector3(0, 100, 0), ForceMode.Impulse);
-            isGrounded = false;
-            animator.SetBool("isJumping", true);
-            
-            animator.SetBool("isJumping", false);
-            isGrounded = true;
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            animator.SetBool("isWalking", true);
-        }
-        moveCharacter();
-        HandleCameraRotation();
         HandleAnimations();
     }
     void moveCharacter()
     {
-        Vector3 worldMovement = transform.TransformDirection(movement);
-        rb.MovePosition(transform.position + worldMovement * playerSpeed * Time.fixedDeltaTime);
-    }
-    private void HandleCameraRotation()
-    {
-        if (Input.GetButton("Fire2"))
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 forward = playerCamera.forward;
+        Vector3 right = playerCamera.right;
+
+        forward.y = 0f; right.y = 0f;
+        forward.Normalize(); right.Normalize();
+
+        moveDirection = (forward * moveZ + right * moveX).normalized;
+
+        Vector3 worldMovement = moveDirection * playerSpeed * Time.deltaTime;
+        rb.AddForce(transform.position + worldMovement * playerSpeed * Time.fixedDeltaTime);
+        //rb.MovePosition(transform.position + worldMovement);
+
+        if (moveDirection != Vector3.zero)
         {
-            yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
-            playerCamera.transform.localRotation = Quaternion.Euler(0, yaw, 0);
+            Quaternion toRotation = Quaternion.LookRotation(moveDirection);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, playerRotation * Time.deltaTime);
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("OnCollisionEnter void works");
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+            Debug.Log($"isGrounded {isGrounded}");
+        }
+        
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("void OnCollisionExit works");
+        if (!collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+            Debug.Log($"isGrounded {isGrounded}");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (sword.transform.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Attacked");
+        }
+        if (sword.transform.CompareTag("Enemy"))
+        {
+            Debug.Log("Attacked 2");
         }
     }
     protected void HandleAnimations()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector3(0, 200, 0), ForceMode.Impulse);
+            animator.SetTrigger("Jump");
+        }
+        if (Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f)
+        {
+            animator.SetBool("isWalking", true);
+            moveCharacter();
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            animator.SetTrigger("Attacks");
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            shieldProtect = true;
+            animator.SetTrigger("Block");
+        }
+        else
+        {
+            shieldProtect = false;
+        }
+    }
+    protected void Attack()
     {
         
     }
